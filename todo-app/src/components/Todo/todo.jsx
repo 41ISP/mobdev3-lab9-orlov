f51f5077778b0b32c8148205cde51d87e992e969
Spl1tVoid <dirton58@gmail.com>
2025-09-24 11:24:50 +0000

    wip

---
+++ todo-app/src/components/Todo/todo.jsx
@@ -0,0 +1,11 @@
+function Todo() {
+    return (
+
+        <div className="todo-item">
+            <input type="checkbox" className="todo-checkbox" />
+            <span className="todo-text">Создать компонентную архитектуру</span>
+            <button className="delete-btn">Удалить</button>
+        </div>
+    )
+}
+export default Todo
